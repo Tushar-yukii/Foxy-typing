77bb9f7892b19974203f153ac4106183e01d5cc9
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-08-16 23:59:29 +0530

    Typing game

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-const words = 'in one good real one not school set they state number word about after need much open also change dolor sit amet consectetur adipisicing elit. Id vel vitae ipsum commodi earum in, voluptates consequatur ratione! Expedita corrupti ut dolor sit reicilon and go will to school'.split('');
+const words =
+  "in one good real one not school set they state number word about after need much open also change dolor sit amet consectetur adipisicing elit. Id vel vitae ipsum commodi earum in, voluptates consequatur ratione! Expedita corrupti ut dolor sit reicilon and go will to school".split(
+    ""
+  );
 const wordscount = words.length;
 
-function randomword(){
-    const randomindex = Math.ceil(Math.random() * wordscount);
-    return words[randomindex];
+function randomword() {
+  const randomindex = Math.ceil(Math.random() * wordscount);
+  return words[randomindex];
 }
 
-function formatword(word){
-   return  
-   `<div class = "word">
+function formatword(word) {
+  return;
+  `<div class = "word">
      <span class="letter">
-        ${word.split('').join('<span class="letter">')}
+        ${word.split("").join('<span class="letter">')}
      </span>
      </span>
    </div>`;
 }
 
 function newgame() {
-    document.getElementById('words').innerHTML = '';
-    for(let i = 0; i < 200; i++){
-       document.getElementById('words').innerHTML += formatword(randomword());
-
-    }
-    document.querySelector('.word')
+  document.getElementById("words").innerHTML = "";
+  for (let i = 0; i < 200; i++) {
+    document.getElementById("words").innerHTML += formatword(randomword());
+  }
+  document.querySelector(".word");
 }
 
-document.getElementById('game').addEventListener('keyup',(ev)=>{
-    const key = ev.key;
-})
-
+document.getElementById("game").addEventListener("keyup", (ev) => {
+  const key = ev.key;
+});
 
 newgame();
